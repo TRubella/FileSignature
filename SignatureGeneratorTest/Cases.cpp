ae297835d50346703d9b03f01a87f3e66de0db23
Panika <t.rubella@yandex.ru>
2016-09-12 16:26:55 +0300

    Some performance testing

--- SignatureGeneratorTest/Cases.cpp
+++ SignatureGeneratorTest/Cases.cpp
@@ -1,4 +1,6 @@
 #define BOOST_TEST_MODULE FileSignatureTestModule
+
+#include <chrono>
 #include <boost/test/unit_test.hpp>
 #include <boost/filesystem/operations.hpp>
 
@@ -85,6 +87,57 @@ BOOST_FIXTURE_TEST_CASE(CRCForSingleBlock, Fixture<512>)
    remove(resPath);
 
    BOOST_CHECK_EQUAL(crc.checksum(), tested);
+}
+
+BOOST_FIXTURE_TEST_CASE(SingleBlockSingleThreadedBenchmark, Fixture<1ull << 20>)
+{
+   auto resPath = Path().parent_path() /= "stream_result";
+
+   auto start = std::chrono::steady_clock::now();
+
+   auto buffer = std::unique_ptr<char[]>(new char[Size()]);
+
+   std::ifstream istream(Path().string());
+   istream.read(buffer.get(), Size());
+   istream.close();
+
+   boost::crc_32_type crc;
+   crc.process_bytes(buffer.get(), Size());
+
+   std::ofstream ostream(resPath.string(), std::ios_base::binary);
+   auto value = crc.checksum();
+   ostream.write(reinterpret_cast<const char*>(&value), sizeof(boost::crc_32_type::value_type));
+   ostream.close();
+
+   auto streamDuration = std::chrono::steady_clock::now() - start;
+
+   remove(resPath);
+   resPath = Path().parent_path() /= "mmf_result";
+
+   start = std::chrono::steady_clock::now();
+   CreateSignature(Path(), resPath, Size());
+   auto mmfDuration = std::chrono::steady_clock::now() - start;
+
+   remove(resPath);
+   BOOST_CHECK_LT( //fails on small sizes (~less than 200KB)
+      std::chrono::duration_cast<std::chrono::milliseconds>(mmfDuration).count(),
+      std::chrono::duration_cast<std::chrono::milliseconds>(streamDuration).count());
+}
+
+BOOST_FIXTURE_TEST_CASE(Profiling, Fixture<10ull<<30>) //10GB source file
+{
+   auto resPath = Path().parent_path() /= "result";
+
+   auto start = std::chrono::steady_clock::now();
+   CreateSignature(Path(), resPath);
+   auto duration = std::chrono::steady_clock::now() - start;
+
+   std::cout << "\n\"CreateSignature\" for file of" << Size() << "bytes takes:\n\t"
+      << std::chrono::duration_cast<std::chrono::minutes>(duration).count() << ":"
+      << std::chrono::duration_cast<std::chrono::seconds>(duration).count() % 60 << ":"
+      << std::chrono::duration_cast<std::chrono::milliseconds>(duration).count() % 1000;
+
+   remove(resPath);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
